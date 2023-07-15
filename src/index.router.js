f26ddcf1a04eabdb21898902c4a5f071a22d20aa
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-07-15 23:17:14 +0300

    routing

--- src/index.router.js
+++ src/index.router.js
@@ -1,7 +1,13 @@
 
 
-const bootstrap=(app,express)=>{
-    app.use(e)
+const bootstrap = (app,express)=>{
+    app.use(express.json())
+    //userRouting
+    //tasktrelloRouting
+    //DB
+    app.use('*',(req,res,next)=>{
+       return res.json({message:"In-Valide Routing❌"})
+    })
 }
 
 export default bootstrap
