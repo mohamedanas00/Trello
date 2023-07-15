7fd8c9c150e8545f8083588e294c3510f65390fa
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-07-15 23:36:20 +0300

    check

--- DB/connection.js
+++ DB/connection.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB=mongoose.connect(`mongodb://localhost:27017/TrelloDB`).then(res=>{
+const connectDB=async ()=>{
+    await mongoose.connect(`mongodb://localhost:27017/TrelloDB`).then(res=>{
     console.log(`ConnectionDB🟩`);
 }).catch(err=>{
     console.log(`Faild..To Connect🟥`);
 })
-
+}
 export default connectDB
