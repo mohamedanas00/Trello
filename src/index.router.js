27a2ff581141a6a41f4680d55e5d78b10f363400
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-07-16 00:47:57 +0300

    errorHandling

--- src/index.router.js
+++ src/index.router.js
@@ -4,6 +4,7 @@ import connectDB from "../DB/connection.js"
 const bootstrap = (app,express)=>{
     app.use(express.json())
     //userRouting
+    // app.use("/user")
     //tasktrelloRouting
     //DB
     connectDB()
---
+++ src/module/utils/errorHandeling.js
@@ -0,0 +1,13 @@
+export const asyncHandler=(fn)=>{
+    return (req,res,next)=>{
+        fn(req,res,next).catch(error=>{
+            return next(new Error(error))
+        })
+    }
+}
+
+export const globalErrorHandling=(error,req,res,next)=>{
+    return res.json({
+    message:"G Error",
+    msgError:error.message})
+}
